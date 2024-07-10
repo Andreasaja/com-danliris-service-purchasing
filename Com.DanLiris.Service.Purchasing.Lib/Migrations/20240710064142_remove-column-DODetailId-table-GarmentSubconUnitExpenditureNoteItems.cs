5041b1e210b67bc10d1fe2a8518319523f9dddc5
Andreasaja <under.block22@gmail.com>
2024-07-10 14:20:44 +0700

    remove-column-DODetailId-table-GarmentSubconUnitExpenditureNoteItems

---
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20240710064142_remove-column-DODetailId-table-GarmentSubconUnitExpenditureNoteItems.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
+{
+    public partial class removecolumnDODetailIdtableGarmentSubconUnitExpenditureNoteItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+               name: "DODetailId",
+               table: "GarmentSubconUnitExpenditureNoteItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
